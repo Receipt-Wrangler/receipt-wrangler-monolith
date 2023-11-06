dff7a96ce287466de53a2a33b9ca2734b6454c3d
Noah <noah231515@gmail.com>
2023-11-06 08:45:31 -0500

    update npmrc path

--- setup-ci.sh
+++ setup-ci.sh
@@ -1,3 +1,3 @@
 # Write .npmrc
-echo "@receipt-wrangler:registry=https://npm.pkg.github.com/" > .npmrc
-echo "//npm.pkg.github.com/:_authToken=$GH_PACKAGE_READ_TOKEN_DESKTOP" > .npmrc
+echo "@receipt-wrangler:registry=https://npm.pkg.github.com/" > /app/receipt-wrangler-desktop/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$GH_PACKAGE_READ_TOKEN_DESKTOP" > /app/receipt-wrangler-desktop/.npmrc
